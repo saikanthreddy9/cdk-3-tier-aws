2d2f1223a9dffd3bad79d63871c0635a5f349e94
saikanthreddy9 <157658134+saikanthreddy9@users.noreply.github.com>
2024-02-23 16:09:18 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# --- root/outputs.tf ---
-
-output "load_balancer_endpoint" {
-  value = module.loadbalancing.lb_endpoint
-}
-
-output "database_endpoint" {
-  value = module.database.db_endpoint
-}
