dca177db9eed54429a9494748a484ec5a1293cfa
saikanthreddy9 <157658134+saikanthreddy9@users.noreply.github.com>
2024-02-23 16:08:38 +0530

    Delete modules/three-tier-deployment directory

--- modules/three-tier-deployment/compute/outputs.tf
+++
@@ -1,9 +0,0 @@
-# --- compute/outputs.tf ---
-
-output "app_asg" {
-  value = aws_autoscaling_group.three_tier_app
-}
-
-output "app_backend_asg" {
-  value = aws_autoscaling_group.three_tier_backend
-}
